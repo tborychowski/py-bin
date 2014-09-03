4e697286e5055e8aeb978fc1d841fc417e7e7c16
Tom <tborychowski@gmail.com>
2014-09-03 11:38:33 +0100

    yweather

---
+++ src/yweather.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+''' python yweather.py | grep 'temp:' | cut -d':' -f2 '''
+
+import requests
+import argparse
+from bs4 import BeautifulSoup
+
+__version__ = '1.0'
+
+def check(args):
+    url = 'http://weather.yahooapis.com/forecastrss?u=c&w=' + args.id
+    # resp = requests.get(url)
+    resp = requests.get(url).text.encode('utf-8')
+    xml = BeautifulSoup(resp)
+
+    loc = xml.find('yweather:location')
+    print('location:' + loc['city'] + ', ' + loc['country'])
+
+    temp = xml.find('yweather:condition')
+    print('desc:' + temp['text'])
+    print('temp:' + temp['temp'])               # + 'C'
+    print('code:' + temp['code'])
+    # print('img: ' + 'http://l.yimg.com/a/i/us/we/52/' + temp['code'] + '.gif')
+    # print('img: ' + 'http://s.imwx.com/v.20131006.215409/img/wxicon/100/' + temp['code'] + '.png')
+
+    wind = xml.find('yweather:wind')
+    print('feelslike:' + wind['chill'])         # + ' C'
+    print('wind:' + wind['speed'])              # + ' km/h'
+
+    athm = xml.find('yweather:atmosphere')
+    print('humidity:' + athm['humidity'])       # + '%'
+    print('pressure:' + athm['pressure'])       # + 'mBar'
+
+    forecast = xml.find_all('yweather:forecast')
+    print('')
+    for i, f in enumerate(forecast):
+        print ('forecast' + str(i) + ':' + f['code'] + '|' + f['low'] + '-' + f['high'] + '|' + f['text'] +
+            '|' + f['day'] + ', ' + f['date'])
+
+
+parser = argparse.ArgumentParser(prog='weather', description='Yahoo Weather CLI')
+parser.add_argument('id', help='WOEID to show weather for', nargs='?', default='560743')        # dublin
+parser.add_argument('-v', '--version', action='version', version='%(prog)s {version}'.format(version=__version__))
+args = parser.parse_args()
+if args.id: check(args)
